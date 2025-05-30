99f1ec5b3f8ddbb59f7b01ba6010b7a77c42ab08
Mikhail <nemkevich.mikhail05@gmail.com>
2025-05-30 10:38:55 +0300

    beta

--- app/src/main/java/com/example/quizapp/presentation/screens/HistoryScreen.kt
+++ app/src/main/java/com/example/quizapp/presentation/screens/HistoryScreen.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Badge
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Divider
